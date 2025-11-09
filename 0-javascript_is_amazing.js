86d9d0393fc24c5aa1ec4f12449b152c6ddf32d8
Joshua Odedeyi <hollyray2003@gmail.com>
2025-11-09 17:42:27 +0100

    added javascript is amazing script

---
+++ 0-javascript_is_amazing.js
@@ -0,0 +1,7 @@
+//Javascript to print Javascript is amazing
+
+//Define Variable Name and Initialize it
+const myVar = 'JavaScript is amazing';
+
+//Print out the Value of myVar
+console.log(myVar);
