d68cd17cd9fcac02a1a2054e5143a23a44c625f7
Joshua Odedeyi <hollyray2003@gmail.com>
2025-11-09 19:03:37 +0100

    javascript to add to arguments integers

---
+++ 9-add.js
@@ -0,0 +1,8 @@
+//Script that prints the addition of 2 integers
+const num1 = parseInt(process.argv[2], 10);
+const num2 = parseInt(process.argv[3], 10);
+
+function add(a, b) {
+    return a + b;
+}
+console.log(add(num1, num2));
