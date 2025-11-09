b7fde781dd5c76dac538cdff3bc44c39023143a8
Joshua Odedeyi <hollyray2003@gmail.com>
2025-11-09 18:35:56 +0100

    javascript to print 3 lines using arrays

---
+++ 6-multi_languages_loop.js
@@ -0,0 +1,5 @@
+//JavaScript that prints 3 lines using array
+let output = ["C is fun", "Python is cool", "JavaScript is amazing"];
+for (let i = 0; i < output.length; i++) {
+    console.log(output[i]);
+}
