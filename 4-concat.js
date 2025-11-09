807210408ef1760fb9cfdebe1239ceefbd43127b
Joshua Odedeyi <hollyray2003@gmail.com>
2025-11-09 18:17:17 +0100

    javascript to concatenate two arguments

---
+++ 4-concat.js
@@ -0,0 +1,9 @@
+//JavaScript script that concatenates two strings
+
+//define variables to hold the two strings from command line arguments
+const str1 = process.argv[2];
+const str2 = process.argv[3];
+
+//print the concatenated result
+console.log(`${str1} is ${str2}`);
+
